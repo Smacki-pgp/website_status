9d71cc9fc013c31ecfa0b92b58bda7772588d575
Smacki-pgp <jocysmodestas1@gmail.com>
2024-11-26 23:00:18 +0200

    Check for https:// if not then include

--- website_status.py
+++ website_status.py
@@ -1,14 +1,24 @@
 import requests
 
 def check_url():
-    url = input("Enter a website url: ")
-    response = requests.get(url)
-    if response.status_code == 200:
+    url = input("Enter a website url: ")# Request url
+    if url == "x": # Just a simple exit point
+            return
+    if "https://" not in url: # Check if Https:// is included
+        response = requests.get("https://" + url)
+    else:
+        response = requests.get(url)
+     
+    if response.status_code == 200: #If url is responsive
         print(f"{url} is reachable! ")
-        loaded_fc = response.text[:500]
-        print(f"\n Page loaded: \n {loaded_fc}")
+        loaded_fc = response.text[:500] #collect first 500 characters in page
+        print(f"\n Page loaded: \n {loaded_fc}") 
     else:
         print(f"Failed to reach {url}. Status code: {response.status_code}")
+   
+        
+
+
 
 
 check_url()
